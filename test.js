e6866d85fe2785ff1eb8a1027002c07910e37de7
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-07 14:37:53 -0700

    Copying over

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import tempLate from "./"
+import allObjectP from "./"
 
-test(({same, end}) => {
-  same(
-    tempLate(true),
-    false
-  )
-
-  end()
+test(({same}) => {
+  return allObjectP({
+    aa: Promise.resolve("aa"),
+    bb: Promise.resolve("bb"),
+  })
+    .then((resolution) => same(
+      resolution, {
+        aa: "aa",
+        bb: "bb"
+      }))
 })
