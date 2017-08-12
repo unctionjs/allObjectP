2f6cfef1a44be5605f65d6a718cde41684f1f7b7
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:13:01 -0700

    More test import fixes

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import  from "./source"
+import allObjectP from "./source"
 
 test(({same}) => {
   return allObjectP({
