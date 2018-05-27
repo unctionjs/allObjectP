9ce5206cc88f5c8d7e0043f775cfb93aa626be42
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-import allP from "@unction/allp"
-import thenP from "@unction/thenp"
-import keys from "@unction/keys"
-import values from "@unction/values"
-import zip from "@unction/zip"
-import pipe from "@unction/pipe"
-import fromArrayToObject from "@unction/fromarraytoobject"
-
-export default function allObjectP (record: RecordType): Promise<RecordType> {
-  return thenP(
-    pipe([zip(keys(record)), fromArrayToObject])
-  )(
-    allP(values(record))
-  )
+import allP from "@unction/allp";
+import thenP from "@unction/thenp";
+import keys from "@unction/keys";
+import values from "@unction/values";
+import zip from "@unction/zip";
+import pipe from "@unction/pipe";
+import fromArrayToObject from "@unction/fromarraytoobject";
+export default function allObjectP(record) {
+  return thenP(pipe([zip(keys(record)), fromArrayToObject]))(allP(values(record)));
 }
