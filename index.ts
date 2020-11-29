ae9698cf251c3ed9737ea310b0891b9cfd0e14ef
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:19 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import values from "@unction/values";
 import zip from "@unction/zip";
 import pipe from "@unction/pipe";
 import fromArrayToObject from "@unction/fromarraytoobject";
-import {ObjectType} from "./types";
 
 export default function allObjectP<T> (record: ObjectType<T>): Promise<ObjectType<T>> {
   return thenP(
